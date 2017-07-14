362f26d55f52da7b2c30a57d60fc293a2231f07c
pesa0015 <pesa0015@gmail.com>
2017-07-14 21:30:51 +0000

    Create user with facebook_id if user doesn't exist

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 // use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use App\User;
 use Illuminate\Http\Request;
 use JWTAuth;
 use Tymon\JWTAuth\Exceptions\JWTException;
@@ -34,6 +35,17 @@ class LoginController extends Controller
 
     public function login(Request $request)
     {
+        $user = User::where('facebook_id', $request->facebook_id)->first();
+
+        if (!$user) {
+            $user = User::create([
+                'facebook_id' => $request->facebook_id,
+                'name' => $request->name,
+                'email' => $request->email,
+                'gender' => $request->gender
+                // 'password' => bcrypt($request->password)
+            ]);
+        }
         // grab credentials from the request
         $credentials = $request->only('email', 'password');
 
