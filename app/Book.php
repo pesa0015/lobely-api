a6e376ab33053f6e3277164cf9bc0a887cf06c96
pesa0015 <pesa0015@gmail.com>
2017-07-14 21:22:20 +0000

    Remove timestamps = false and fix psr2

--- app/Book.php
+++ app/Book.php
@@ -6,8 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-	public $timestamps = false;
-
     protected $fillable = [
         'official_title', 'author', 'publisher', 'published_year', 'pages', 'language', 'isbn_nr'
     ];
--- app/Bookshelf.php
+++ app/Bookshelf.php
@@ -14,11 +14,11 @@ class Bookshelf extends Model
 
     public function user()
     {
-    	return $this->belongsTo('App\User', 'user_id');
+        return $this->belongsTo('App\User', 'user_id');
     }
 
     public function book()
     {
-    	return $this->belongsTo('App\Book', 'book_id');
+        return $this->belongsTo('App\Book', 'book_id');
     }
 }
