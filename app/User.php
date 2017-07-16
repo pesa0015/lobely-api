2eee8a8d567b2764cfe4c1078e2572588795c8d5
pesa0015 <pesa0015@gmail.com>
2017-07-16 09:58:46 +0000

    Add fillable facebook_id

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'gender', 'interested_in_gender', 'password',
+        'facebook_id', 'name', 'email', 'gender', 'interested_in_gender', 'password',
     ];
 
     /**
