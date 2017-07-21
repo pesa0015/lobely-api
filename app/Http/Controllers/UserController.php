3f30cfa9d8ba8c095bbcb719e662dfe6dccb2326
pesa0015 <pesa0015@gmail.com>
2017-07-21 20:19:09 +0000

    Remove old method from UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,30 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Auth;
-use App\Bookshelf;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function dashboard()
-    {
-        $my_books = Bookshelf::with('book')->where('user_id', Auth::user()->id)->get();
-        return view('bookshelf', ['my_books' => $my_books]);
-    }
+    
 }
