a7e53cfd3e4a59cdf904058a35fc3ecb61d8cecd
pesa0015 <pesa0015@gmail.com>
2017-09-15 18:51:35 +0000

    Reset passwords: Fix migration for users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -36,6 +36,16 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
+        if (Schema::hasTable('reset_passwords')) {
+            Schema::table('reset_passwords', function (Blueprint $table) {
+                $table->dropForeign('reset_passwords_user_id_foreign');
+            });
+        } elseif (Schema::hasTable('password_resets')) {
+            Schema::table('password_resets', function (Blueprint $table) {
+                $table->dropForeign('password_resets_user_id_foreign');
+            });
+        }
+
         Schema::drop('users');
     }
 }
