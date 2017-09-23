6f15e4ac14f6e6d748a99aca58cc74ccb2944758
pesa0015 <pesa0015@gmail.com>
2017-09-23 20:22:46 +0000

    Remove web-routes

--- routes/web.php
+++ routes/web.php
@@ -10,13 +10,3 @@
 | to using a Closure or controller method. Build something great!
 |
 */
-
-Route::get('bookshelf', 'UserController@dashboard');
-Route::get('book/{slug}', 'BookController@showBook');
-Route::post('save-to-bookshelf', 'BookController@save');
-Route::post('remove-from-bookshelf', 'BookController@remove');
-
-Route::get('auth/facebook', 'Auth\LoginController@redirectToProvider');
-Route::get('auth/facebook/callback', 'Auth\LoginController@handleProviderCallback');
-
-Auth::routes();
