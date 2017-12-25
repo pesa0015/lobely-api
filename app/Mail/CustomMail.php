fd4296e1214840479dce992515795cd6c2272eab
pesa0015 <pesa0015@gmail.com>
2017-12-25 17:37:54 +0000

    Add to in mail

--- app/Mail/CustomMail.php
+++ app/Mail/CustomMail.php
@@ -8,7 +8,7 @@ class CustomMail
 {
     public function send($view, $to, array $data = [])
     {
-        Mail::send($view, $data, function ($message) {
+        Mail::send($view, $data, function ($message) use ($to) {
             $message->from(env('MAIL_ADDRESS'), env('MAIL_NAME'));
             $message->to($to);
         });
