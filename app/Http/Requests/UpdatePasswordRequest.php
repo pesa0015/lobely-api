bb66d81f0a10597ec676bfcf58aeeee26b3eb64a
pesa0015 <pesa0015@gmail.com>
2017-12-25 18:04:45 +0000

    Add UpdatePasswordRequest

---
+++ app/Http/Requests/UpdatePasswordRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Http\Requests\Request;
+
+class UpdatePasswordRequest extends Request
+{
+    protected $rules = [
+        'current'   => 'required|string',
+        'new'       => 'required|min:5',
+        'repeatNew' => 'required|min:5'
+    ];
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return $this->rules;
+    }
+}
