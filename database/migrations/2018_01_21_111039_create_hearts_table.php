b6db9cac1b1e0ca06262785f12c66d5cac1cedaa
pesa0015 <pesa0015@gmail.com>
2018-01-22 17:42:42 +0000

    Add migration for create hearts table

---
+++ database/migrations/2018_01_21_111039_create_hearts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHeartsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hearts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned()->nullable();
+            $table->integer('heart_user_id')->unsigned()->nullable();
+            $table->integer('book_id')->unsigned()->nullable();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('heart_user_id')->references('id')->on('users');
+            $table->foreign('book_id')->references('id')->on('books');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hearts');
+    }
+}
